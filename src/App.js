74591b19642dcad4ac15e8793a3b168cb4c38177
ishz0 <girdhar.kataria@einfochips.com>
2022-07-22 18:17:10 +0530

    react-crud

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
+import Nav from './components/Nav';
 import './App.css';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import Footer from './components/Footer';
+import SignUp from './components/SignUp';
+import PrivateComponent from './components/PrivateComponent';
+import Login from './components/Login';
+import AddShow from './components/AddShow';
+import ShowList from './components/ShowList';
+import UpdateShow from './components/UpdateShow';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+      <Nav />
+      <Routes>
+        <Route element={<PrivateComponent/>}>
+        <Route path='/' element={<ShowList />}/>
+        <Route path="/add" element={<AddShow />} />
+        <Route path='/update/:id' element={<UpdateShow />}/>
+        <Route path='/logout' element={<h1>Logout</h1>}/>
+        </Route>
+        <Route path='/signup' element={<SignUp />}/>
+        <Route path='/login' element={<Login/>}/>
+      </Routes>
+      </BrowserRouter>
+      <Footer />
     </div>
   );
 }
